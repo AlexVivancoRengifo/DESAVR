cb622c62f73ff427724cbe4c0c6b8e99e87b62d7
Alex Vivanco <deisvra@gmail.com>
2017-12-18 02:02:13 +0000

    feature file updated

--- src/main/java/HandskullWebPageSteps.java
+++ src/main/java/HandskullWebPageSteps.java
@@ -2,9 +2,11 @@
  * Created by C0225667 on 18/12/2017.
  */
 public class HandskullWebPageSteps {
-    @cucumber.api.java.en.Given("^a user \"([^\"]*)\" with password \"([^\"]*)\"$")
-    public void aUserWithPassword(String arg0, String arg1) throws Throwable {
+
+    @Given("^I should see the login form$")
+    public void i_should_see_the_login_form() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        //        throw new PendingException();
+        System.out.println("Hello World");
     }
 }
